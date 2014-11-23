bcc9401d988cfcd0a2425c56fcb996ea82b7f666
longtaoge <61852263@qq.com>
2014-11-23 22:53:20 +0800

    screenshot
    screenshot

---
+++ TabActivtity/src/org/xiangbalao/HomeActivity.java
@@ -0,0 +1,18 @@
+package org.xiangbalao;
+
+import org.xiangbalao.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author Adil Soomro
+ *
+ */
+public class HomeActivity extends Activity {
+	
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);      
+    }
+}
